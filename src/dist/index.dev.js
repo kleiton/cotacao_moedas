9ffbabfc6b9a8c3cd8babe20a3bc7412431ee921
Kleiton Oliveira <mor.kleiton@gmail.com>
2020-06-12 09:11:31 -0300

    Carga de dados do WS, conversão para XML e ajustes

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,16 +1,29 @@
 "use strict";
 
+/**
+ * @param regexXMLInicio procura pelo padrão <intervalo_de_numero> em toda a linha (g), em todo o texto (m)
+ * @param regexXMLFim procura pelo padrão </intervalo_de_numero> em toda a linha (g), em todo o texto (m)
+ * @param dados retorno assincrono da função getContacao()
+ * @param valores armazena os valores retornados no JSON da função getCotacao()
+ * @param options armazena parametros opcionais necessários para rodar a função de conversão JSON / OBJECT / XML
+ * @param diretorio Diretório onde o arquivo final será salvo
+ * @param resultado Resultado final dos dados retornados pela função getCotacao() e posteriormente convertido para XML com ajustes em regex
+ * @param data Armazena o dia atual
+ * @param dataInicial Periodo Inicial que o WS irá retornar os dados
+ * @param dataFinal Periodo Final que o WS irá retornar os dados
+ * @param link Link para acesso ao WS ja contendo os valores das variáveis acima.
+ */
+var cron = require("node-cron");
+
 var axios = require('axios');
 
 var fs = require('fs');
 
 var convert = require('xml-js');
 
-var moeda = 'USD';
-var dias = 3;
-var dataInicial = '01-01-2019';
+var dataInicial = '01-01-1990';
 var dataFinal = '12-31-2020';
-var link = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='".concat(dataInicial, "'&@dataFinalCotacao='").concat(dataFinal, "'&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao");
+var link = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%27".concat(dataInicial, "%27&@dataFinalCotacao=%27").concat(dataFinal, "%27&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao");
 
 var getCotacao = function getCotacao() {
   return regeneratorRuntime.async(function getCotacao$(_context) {
@@ -38,7 +51,7 @@ var getCotacao = function getCotacao() {
 };
 
 var retornaCotacao = function retornaCotacao() {
-  var dados, valores, options, result;
+  var dados, valores, options, resultado, diretorio, regexXMLInicio, regexXMLFim, data;
   return regeneratorRuntime.async(function retornaCotacao$(_context2) {
     while (1) {
       switch (_context2.prev = _context2.next) {
@@ -52,21 +65,36 @@ var retornaCotacao = function retornaCotacao() {
           options = {
             compact: true,
             spaces: 4,
-            ignoreText: true
+            ignoreText: false
           };
-          result = convert.js2xml(valores, options);
-          console.log(result);
-          fs.writeFile("cotacoes.txt", convert.js2xml(valores, options), function (err) {
+          resultado = convert.js2xml(valores, options);
+          diretorio = "/\SF009144/\Lucas/\GDP";
+          regexXMLInicio = /<[0-9]+>/gm;
+          regexXMLFim = /<\/[0-9]+>/gm;
+          data = new Date(); //São aplicadas alterações nos dados retornados pelo WS, isso é necessário para que o Qlikview entenda a esrutura dos dados
+
+          resultado = '<COTACAO>' + '\n' + resultado.replace(regexXMLInicio, '<COTACOES>').replace(regexXMLFim, '</COTACOES>') + '\n' + '</COTACAO>';
+          /**
+           * O resultado final será armazenado no arquivo cotacoes.xml
+           */
+
+          fs.writeFile('/\/\SF009144/\Lucas/\GDP/\cotacoes.xml', resultado, function (err) {
             if (err) return console.log(err);
-            console.log("Cota\xE7\xF5es > cotacoes.txt");
+            console.log("Arquivo cotacoes.xml gerado no diret\xF3rio ".concat(diretorio, " em ").concat(data));
           });
 
-        case 8:
+        case 12:
         case "end":
           return _context2.stop();
       }
     }
   });
 };
+/**
+ * Os dados serão gerados diariamente as 18 horas
+ */
 
-retornaCotacao();
+
+cron.schedule('0 18 * * *', function (ctx) {
+  retornaCotacao();
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+/**
+ * @param regexXMLInicio procura pelo padrão <intervalo_de_numero> em toda a linha (g), em todo o texto (m)
+ * @param regexXMLFim procura pelo padrão </intervalo_de_numero> em toda a linha (g), em todo o texto (m)
+ * @param dados retorno assincrono da função getContacao()
+ * @param valores armazena os valores retornados no JSON da função getCotacao()
+ * @param options armazena parametros opcionais necessários para rodar a função de conversão JSON / OBJECT / XML
+ * @param diretorio Diretório onde o arquivo final será salvo
+ * @param resultado Resultado final dos dados retornados pela função getCotacao() e posteriormente convertido para XML com ajustes em regex
+ * @param data Armazena o dia atual
+ * @param dataInicial Periodo Inicial que o WS irá retornar os dados
+ * @param dataFinal Periodo Final que o WS irá retornar os dados
+ * @param link Link para acesso ao WS ja contendo os valores das variáveis acima.
+ */
+
+const cron = require("node-cron");
 const axios = require('axios')
 const fs = require('fs');
 const convert = require('xml-js');
 
-const moeda = 'USD';
-const dias = 3;
-const dataInicial = '01-01-2019';
+const dataInicial = '01-01-1990';
 const dataFinal = '12-31-2020';
-let link = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='${dataInicial}'&@dataFinalCotacao='${dataFinal}'&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`;
+let link = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%27${dataInicial}%27&@dataFinalCotacao=%27${dataFinal}%27&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`;
 
 
 const getCotacao = async () => {
@@ -20,18 +33,30 @@ const getCotacao = async () => {
 const retornaCotacao = async () => {
     const dados = await getCotacao()
     let valores = dados.data.value;
-    var options = {compact: true, spaces: 4, ignoreText: true};
-    let result = convert.js2xml(valores, options);
-    console.log(result);
+    const options = { compact: true, spaces: 4, ignoreText: false };
+    let resultado = convert.js2xml(valores, options);
+    const diretorio = "/\SF009144/\Lucas/\GDP";
+    const regexXMLInicio = /<[0-9]+>/gm
+    const regexXMLFim = /<\/[0-9]+>/gm
+    let data = new Date();
 
+    //São aplicadas alterações nos dados retornados pelo WS, isso é necessário para que o Qlikview entenda a esrutura dos dados
+    resultado = '<COTACAO>' + '\n' + resultado.replace(regexXMLInicio, '<COTACOES>').replace(regexXMLFim, '</COTACOES>') + '\n' + '</COTACAO>'
 
-    fs.writeFile(`cotacoes.txt`, convert.js2xml(valores, options), function (err) {
+    /**
+     * O resultado final será armazenado no arquivo cotacoes.xml
+     */
+    fs.writeFile('/\/\SF009144/\Lucas/\GDP/\cotacoes.xml', resultado, function (err) {
         if (err) return console.log(err);
-        console.log(`Cotações > cotacoes.txt`);
-      });
-    
+        console.log(`Arquivo cotacoes.xml gerado no diretório ${diretorio} em ${data}`);
+    });
+
 }
-    
+    ;
+/**
+ * Os dados serão gerados diariamente as 18 horas
+ */
+cron.schedule('0 18 * * *', (ctx) => {
+    retornaCotacao()
 
-
-retornaCotacao()
+})
